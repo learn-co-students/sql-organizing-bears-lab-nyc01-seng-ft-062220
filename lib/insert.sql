07f9531c04b75bf12e5e531a73a4f46692f06eef
orlandofat <orlandofat@gmail.com>
2020-06-30 17:35:37 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 4, "M", "Brown", "laid_back", true);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, "Rowdy", 20, "M", "Black", "laid_back", true);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, "Tabitha", 15, "F", "Brown", "laid_back", true);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, "Sergeant Brown", 4, "M", "Brown", "laid_back", false);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, "Melissa", 6, "F", "Black", "laid_back", true);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6, "Grinch", 9, "M", "Black", "laid_back", true);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, "Wendy", 7, "F", "Brown", "laid_back", false);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8, NULL, 4,"M", "Brown", "laid_back", true);
