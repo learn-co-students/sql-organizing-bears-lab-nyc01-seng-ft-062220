fced20de2f720ac7f61a65eedf7fb34712be1583
catd825 <catd825@gmail.com>
2020-07-01 11:51:54 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears (name, gender) VALUES ("Mr. Chocolate", "M");
+INSERT INTO bears (name, gender) VALUES ("Rowdy", "F");
+INSERT INTO bears (name, gender) VALUES ("Tabitha", "M");
+INSERT INTO bears (name, gender) VALUES ("Sergeant Brown", "M");
+INSERT INTO bears (name, gender) VALUES ("Melissa", "F");
+INSERT INTO bears (name, gender) VALUES ("Grinch", "M");
+INSERT INTO bears (name, gender) VALUES ("Wendy", "F");
+INSERT INTO bears (name, gender) VALUES (NULL, "M")
+
+
+-- INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
