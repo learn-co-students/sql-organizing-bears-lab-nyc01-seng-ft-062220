3fa955fabba6b327ef98e6fb998ae6b67468f124
yulia <juliana.ny2008@gmail.com>
2020-07-01 21:18:12 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO BEARS(id, name, age, gender, color, temperament, alive) VALUES(11, "Mr. Chocolate", 2, "M", "white", "great", true);
+INSERT INTO BEARS(id, name, age, gender, color, temperament, alive) VALUES(22, "Rowdy", 2, "M", "white", "great", true);
+INSERT INTO BEARS(id, name, age, gender, color, temperament, alive) VALUES(44, "Tabitha", 2, "F", "white", "great", true);
+INSERT INTO BEARS(id, name, age, gender, color, temperament, alive) VALUES(55, "Sergeant Brown", 2, "M", "white", "great", true);
+INSERT INTO BEARS(id, name, age, gender, color, temperament, alive) VALUES(66, "Melissa", 8, "F", "white", "great", false);
+INSERT INTO BEARS(id, name, age, gender, color, temperament, alive) VALUES(77, "Grinch", 5, "M", "white", "great", true);
+INSERT INTO BEARS(id, name, age, gender, color, temperament, alive) VALUES(88, "Wendy", 2, "F", "dark brown", "great", true);
+INSERT INTO BEARS(id, name, age, gender, color, temperament, alive) VALUES(33, NULL, 2, "M", "white", "great", true);
