b6e4b70b431e700613115bea66a32762cb0ecc6a
“jzhouerm” <jzhouerm@hotmail.comgit config --global user.name jzhouermgit config --global user.email jzhouerm@hotmail.com>
2020-07-03 17:40:46 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,gender) VALUES ('Mr.Chocolate', 'F');
+INSERT INTO bears (name,gender) VALUES ('Rowdy', 'F');
+INSERT INTO bears (name,gender) VALUES ('Tabitha', 'M');
+INSERT INTO bears (name,gender) VALUES ('Mr.Sergeant Brown', 'M');
+INSERT INTO bears (name,gender) VALUES ('Melissa', 'F');
+INSERT INTO bears (name,gender) VALUES ('Grinch', 'M');
+INSERT INTO bears (name,gender) VALUES ('Wendy', 'F');
+INSERT INTO bears (name,gender) VALUES (NULL, 'M');
