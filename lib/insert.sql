7c3186f37c23d8d3831da0708bc29447616fb033
roman9891 <roman9891@gmail.com>
2020-07-05 14:13:54 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Chonky", 5, "M", "brown", "hungry", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 2, "M", "very brown", "strange", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Honey", 3, "F", "less brown", "sassy", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Gorbechev", 1, "M", "golden", "domineering", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Belinda", 2, "F", "green", "caustic", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Victor", 2, "M", "brownless", "calm", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tina", 4, "F", "red", "nice", TRUE);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Artemis", 2, "F", "bleached", "overly emotional", TRUE);
